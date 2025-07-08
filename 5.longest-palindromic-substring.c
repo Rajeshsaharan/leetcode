e1e698c66255296151e7848eaf65a2836748e46c
rajeshsaharan <rajeshsharan1607@gmail.com>
2025-07-08 20:08:24 +0530

    leetcode

---
+++ 5.longest-palindromic-substring.c
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=5 lang=c
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (35.96%)
+ * Likes:    30995
+ * Dislikes: 1909
+ * Total Accepted:    3.9M
+ * Total Submissions: 11M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+char* longestPalindrome(char* s) {
+    
+}
+// @lc code=end
+
