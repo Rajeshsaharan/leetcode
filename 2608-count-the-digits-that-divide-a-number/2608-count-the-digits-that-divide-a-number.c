db8739a4d11867497d1f396b0095af11648e516d
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-08-24 16:59:19 +0530

    Time: 0 ms (100%), Space: 7.7 MB (93.6%) - LeetHub

--- 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.c
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.c
@@ -1,14 +1,6 @@
-void print(int* arr, int size) {
-    int i = 0;
-    while (i < size) {
-        printf("%d\n", arr[i]);
-        i++;
-    }
-}
 
 int countDigits(int num) {
-    int count = 0;
-    int digit = num;
+    int count = 0 , digit = num , result = 0;
     while (digit >= 1) {
         digit /= 10;
         count++;
@@ -21,11 +13,8 @@ int countDigits(int num) {
         digit /= 10;
     }
     i = 0;
-    int result = 0;
     while (i < count) {
-        if (num % divider[i] == 0)
-            result++;
-        i++;
+        if (num % divider[i++] == 0) result++;
     }
     return result;
 }
