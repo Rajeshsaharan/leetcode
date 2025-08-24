7de35b892cf9f5b9eb4a1ff2d06e3b93c5d52c73
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-08-24 17:07:15 +0530

    Time: 0 ms (100%), Space: 7.7 MB (93.6%) - LeetHub

--- 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.c
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.c
@@ -1,20 +1,13 @@
-
+void helper(int num, int * result, int digit){
+    if(digit < 1) return;
+    int divider = digit % 10;
+    if(num % divider ==0) (*result)++;
+    digit /= 10;
+    helper(num, result, digit);
+}
 int countDigits(int num) {
-    int count = 0 , digit = num , result = 0;
-    while (digit >= 1) {
-        digit /= 10;
-        count++;
-    }
-    int divider[count];
-    int i = 0;
-    digit = num;
-    while (digit >= 1) {
-        divider[i++] = digit % 10;
-        digit /= 10;
-    }
-    i = 0;
-    while (i < count) {
-        if (num % divider[i++] == 0) result++;
-    }
-    return result;
+   int digit = num;
+   int result = 0;
+   helper(num, &result, digit);
+   return result;
 }
