377330e341953035fc3f878a2277877818d59b8f
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-08-24 14:22:43 +0530

    Time: 0 ms (100%), Space: 19.7 MB (20.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -8,15 +8,13 @@ void swap(int * a ,int *b){
 void moveZeroes(int* nums, int numsSize) {
     int zp = 0; // zp pointer points to zero element
     int nzp = 0; //nzp pointer points to non zero element
-    int i = 0;
     //finding first nzp value
-    while(i < numsSize){
+    while(nzp < numsSize){
         if(nums[nzp] != 0){
             swap(nums + zp, nums+nzp);
             zp++;
         }
         nzp++;
-        i++;
     }
 
 }
