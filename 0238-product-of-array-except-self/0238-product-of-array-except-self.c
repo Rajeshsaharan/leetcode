26a8b9ff807db14a7eb858a0a8e8ed2f74ee8cad
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-08-28 20:18:35 +0530

    Time: 0 ms (100%), Space: 30.5 MB (22.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.c
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.c
@@ -2,36 +2,29 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
-void print(int * arr, int n){
+void print(int* arr, int n) {
     int i = 0;
-    while(i < n){
-        printf("index %d and item -> %d\n", i ,arr[i]);
+    while (i < n) {
+        printf("index %d and item -> %d\n", i, arr[i]);
         i++;
     }
 }
 
-
-
-
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
-   int left_perfix[numsSize];
-   int right_perfix[numsSize];
-   left_perfix[0] = 1; int  i =1;
-   while(i< numsSize){
-    left_perfix[i] = left_perfix[i-1] * nums[i-1];
-    i++;
-   }
-   print(left_perfix , numsSize);
-   // [1,1,2,6]
-    right_perfix[numsSize -1] = 1; i = numsSize -2;
-    while(i >= 0){
-        right_perfix[i] = right_perfix[i+1] * nums[i+1];
+    int left_perfix[numsSize], right_perfix[numsSize], i;
+    left_perfix[0] = 1,i = 1;
+    while (i < numsSize) {
+        left_perfix[i] = left_perfix[i - 1] * nums[i - 1];
+        i++;
+    }
+    right_perfix[numsSize - 1] = 1,i = numsSize - 2;
+    while (i >= 0) {
+        right_perfix[i] = right_perfix[i + 1] * nums[i + 1];
         i--;
     }
-    print(right_perfix, numsSize);
-    int  * result = (int * ) malloc(numsSize * sizeof(int));
-    i = 0 ;
-    while(i < numsSize){
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    i = 0;
+    while (i < numsSize) {
         result[i] = left_perfix[i] * right_perfix[i];
         i++;
     }
