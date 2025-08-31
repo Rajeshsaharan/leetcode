4251c14be0e259449e4b08af45e9f55c1ec0f207
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-08-31 15:54:45 +0530

    Time: 51 ms (1.25%), Space: 8.3 MB (9.67%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,31 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+    int size = strlen(strs[0]);
+    if (size == 0) return "";
+    char * arr = malloc(size+1 * sizeof(char));
+    arr[0] = '\0';
+    int i =0;
+    while(strs[0][i] != '\0'){
+        arr[i] = strs[0][i];
+        printf("%c\n", arr[i]);
+        i++;
+    }
+    arr[i] = '\0';
+    i =1;
+    while(i < strsSize){
+        int j = 0;
+        while(strs[i][j] != '\0' && j < size ){
+            if(strs[i][j] == arr[j]){
+                printf("%c\n", arr[j]);
+            }
+            else{
+                printf("ending index -> %d", j);
+                arr[j] = '\0';
+                break;
+            }
+            j++;
+        }
+        arr[j] = '\0';
+    i++;
+    }
+    return arr;
+}
