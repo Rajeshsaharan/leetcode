a778a270a9bdea187386ff66bc431227642d9acc
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-05 21:00:07 +0530

    Time: 0 ms (100%), Space: 7.8 MB (40.3%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.c
+++ 0507-perfect-number/0507-perfect-number.c
@@ -2,14 +2,14 @@
 
 bool checkPerfectNumber(int num) {
     if (num <= 1) return false;
-    int total = 1;  
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
+    int total = 1;  //1 and number itself always a divisors
+    int i =2;
+    while(i * i <= num){
+        if(num % i== 0) {
             total += i;
-            if (i != num / i) { 
-                total += num / i;
-            }
+            if(i != num/ i) total += num/i;
         }
+        i++;
     }
     return total == num;
 }
