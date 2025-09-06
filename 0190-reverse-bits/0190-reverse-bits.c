b48881c9e6e63943bb8168ff3eee3aac62fa9009
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-06 19:10:00 +0530

    Time: 5 ms (14.52%), Space: 8.2 MB (52.19%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.c
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -1,6 +1,5 @@
 int reverseBits(int n) {
-    int count = 31, result =0;
-    int i =0;
+    int count = 31, result =0, i =0;
     while(count >= 0){
         unsigned mask = 1u << i++;
         if(n & mask) result |= 1 << count;
