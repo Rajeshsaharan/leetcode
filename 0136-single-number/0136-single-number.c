8febbf6644feacb653f84f347053baf576c4862d
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-08 22:57:47 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.46%) - LeetHub

--- 0136-single-number/0136-single-number.c
+++ 0136-single-number/0136-single-number.c
@@ -1,5 +1,24 @@
 int singleNumber(int* nums, int numsSize) {
-    int i =0, result = 0;
-    while(i < numsSize) result ^= nums[i++];
-    return result;
+    int result  = 0, i =0;
+
+    __asm__ volatile (
+        "mov %0 , %%eax;"
+        "mov %[i] , %%edx;" // use named variable
+        "1:\n\t"
+        "cmp %%esi ,%%eax;"
+        "jge 2f;"
+        "mov (%%rdi, %%rax ,4) ,%%rcx;"
+        "xor %%ecx , %%edx;"
+        "inc %%eax;"
+        "jmp 1b;"
+
+        "2:\n\t"
+        "mov %%edx , %0;" // start output to 0 1 2 3
+        :"+r"(result) // =r (write) + read and write
+        :[i]"r"(i), "D"(nums), "S"(numsSize) //named variable only read 
+        : "rcx", "rdx", "memory");
+
+
+
+    return result;  // here compiler will just use eax already
 }
