43d031aeead71ac80ffafe5fc2dab9358b9d7508
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-09 18:00:02 +0530

    Time: 1 ms (44.44%), Space: 10.4 MB (38.12%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.c
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.c
@@ -5,13 +5,10 @@ int rangeBitwiseAnd(int left, int right) {
         unsigned mask = 1u << i;
         int left_masked = left & mask;
         int  right_masked = right & mask;
-          printf("i -> %d and for mask1 %d and mask2 %d\n", i, left & mask, right & mask);
         if(left_masked == right_masked){
             if(left_masked == 0){
-                printf("%d, 0\n", i);
                 result |= 0u <<i;
             }else{
-                printf("%d, 1\n", i);
                 result |= 1u << i;
             }
             
