93de94a1ebab2f2ec9a8fbd39f54518906661bf4
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-09 18:19:07 +0530

    Time: 0 ms (100%), Space: 10.3 MB (80.85%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.c
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.c
@@ -1,14 +1,29 @@
+unsigned int left_shift(unsigned int bin, int pos) {
+    unsigned int result;
+    __asm__ volatile(
+        "mov %1, %0;"  
+        "shl %%cl, %0;"
+        : "=&r"(result)
+        : "r"(bin), "c"(pos) //c for flag cl register
+    );
+    return result;
+}
+
+
+
+
+
 int rangeBitwiseAnd(int left, int right) {
     unsigned result = 0;
     int i = 31;
     while(i >= 0){
-        int left_masked = left & 1u << i;
-        int  right_masked = right & 1u << i;
+        int left_masked = left & left_shift(1u, i);
+        int  right_masked = right & left_shift(1u, i);
         if(left_masked == right_masked){
             if(left_masked == 0){
-                result |= 0u <<i;
+                result |= left_shift(0u , i);
             }else{
-                result |= 1u << i;
+                result |=left_shift(1u, i);
             }
             
         }else{
