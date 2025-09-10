f454d3baffb184729c5c6f02b24bd19a0316d019
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-10 22:27:03 +0530

    Time: 0 ms (100%), Space: 8 MB (16.42%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.c
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.c
@@ -1,16 +1,13 @@
 int check_ballon(int * arr, int * index){
     int i = 0;
-    int a_value = arr[index[i]]; printf("a_value %d\n", a_value);
+    int a_value = arr[index[i]];
     int b_value = arr[index[i+1]];
     int l_value = arr[index[i+2]];
     int n_value = arr[index[i+3]];
     int o_value = arr[index[i+4]];
     i =0;
     if(a_value >=1 && b_value >=1 && l_value >=2 && n_value >=1 && o_value >=2){
-        arr[index[i]] = a_value -1 ; printf(" must be zero -> %d", arr[index[0]]);
-        arr[index[i+1]] = b_value -1 ;
-        arr[index[i+2]] = l_value -2;
-        arr[index[i+3]] = n_value -1;
+        arr[index[i]]-- ,arr[index[i+1]]-- , arr[index[i+2]] = l_value -2, arr[index[i+3]]--;
         arr[index[i+4]] = o_value -2 ;
         return 1;
     }
