ca31baba070b6ded16f7ab62dfcd42a20f61f7a1
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-10 22:22:16 +0530

    Time: 3 ms (15.67%), Space: 7.9 MB (42.54%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.c
@@ -0,0 +1,37 @@
+int check_ballon(int * arr, int * index){
+    int i = 0;
+    int a_value = arr[index[i]]; printf("a_value %d\n", a_value);
+    int b_value = arr[index[i+1]];
+    int l_value = arr[index[i+2]];
+    int n_value = arr[index[i+3]];
+    int o_value = arr[index[i+4]];
+    i =0;
+    if(a_value >=1 && b_value >=1 && l_value >=2 && n_value >=1 && o_value >=2){
+        arr[index[i]] = a_value -1 ; printf(" must be zero -> %d", arr[index[0]]);
+        arr[index[i+1]] = b_value -1 ;
+        arr[index[i+2]] = l_value -2;
+        arr[index[i+3]] = n_value -1;
+        arr[index[i+4]] = o_value -2 ;
+        return 1;
+    }
+    return 0;
+}
+
+
+int maxNumberOfBalloons(char* text) {
+    int arr[26];
+    int index[] ={0,1, 11, 13, 14}; //a b l n o
+    int i = 0;
+    while(text[i] != '\0'){
+        //printf("%d\n", text[i]-97);
+        arr[text[i] - 97]++;
+        i++;
+    }
+    int count = 0, terminate = 1;
+    while(terminate != 0){
+        int result = check_ballon(arr, index);
+        if(result == 0) terminate = 0;
+        else count++;
+    }
+    return count;
+}
