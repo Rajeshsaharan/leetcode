51920927d91bec006119fb1ec631e586e81e9d9e
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-11 22:31:13 +0530

    Time: 0 ms (100%), Space: 8.1 MB (59.15%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.c
+++ 0205-isomorphic-strings/0205-isomorphic-strings.c
@@ -1,7 +1,5 @@
 bool isIsomorphic(char* s, char* t) {
-  int arr1[128];
-  int arr2[128];
-    int i = 0;
+  int arr1[128], arr2[128] , i = 0;
     while(s[i] != '\0'){
         if(arr1[s[i]] == 0 && arr2[t[i]] == 0){
             arr1[s[i]] = t[i];
