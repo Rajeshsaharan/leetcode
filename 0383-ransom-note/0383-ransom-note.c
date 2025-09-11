1a3aef8392e7478fe3315ae866204de0d01d8631
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-12 04:49:01 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.55%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.c
+++ 0383-ransom-note/0383-ransom-note.c
@@ -1,15 +1,11 @@
 bool canConstruct(char* ransomNote, char* magazine) {
     char * a = ransomNote , *b = magazine;
     int  arr[26], i =0;
-    while(b[i] != '\0'){
-        arr[b[i] - 97]++;
-        i++;
-    }
+    while(b[i] != '\0') arr[b[i++] - 97]++;
     i = 0;
     while(a[i] != '\0'){
          if(arr[a[i]-97] < 1) return false;
-         arr[a[i]-97]--;
-         i++;
+         arr[a[i++]-97]--;
     }
     return true;
 }
