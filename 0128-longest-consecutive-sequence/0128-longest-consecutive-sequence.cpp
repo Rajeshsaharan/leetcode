54ef7688b1789e7b58bb8a5fd968d11317f72fe3
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-14 08:26:02 +0530

    Time: 1345 ms (5.05%), Space: 65.3 MB (75.11%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        priority_queue<int, vector<int>, greater<int>> minHeap; 
+        int i =0;
+        while(i < nums.size()){
+            minHeap.push(nums[i]);
+            i++;
+        }
+        int count = 0;
+        int max = INT_MIN;
+        int first = minHeap.top();
+        minHeap.pop();
+        while(!minHeap.empty()){
+            cout << minHeap.top() << " ";
+            int second = minHeap.top();
+            if(first == second){
+                minHeap.pop();
+                continue;
+            }else if( first +1 == second){
+                count++;
+                printf("count%d\n", count);
+            }else{
+                max = fmax(max, count);
+                count = 0;
+            }
+            first = second;
+            minHeap.pop();
+        }
+        max = fmax(max, count);
+        return max+1;
+    }
+};
