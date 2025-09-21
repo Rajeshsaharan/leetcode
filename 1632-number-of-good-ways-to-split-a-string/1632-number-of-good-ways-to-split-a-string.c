2ee05814175c2827e901f82140484a1b8ad5a77e
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-21 16:23:53 +0530

    Time: 0 ms (100%), Space: 9.6 MB (0%) - LeetHub

--- 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.c
+++ 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.c
@@ -1,51 +1,39 @@
 int numSplits(char* s) {
     int size = strlen(s);
-    int freq1[26] = {0};
-    int freq2[26] = {0};
-    int i = 0;
-    int count = 0;
+    int freq1[26] = {0} ,freq2[26] = {0};
+    int i = 0 , count = 0;
     while (s[i] != '\0') {
         if(freq1[s[i]- 97] > 0) count++;
         freq1[s[i] - 'a']++;
         freq2[s[i]-'a']++;
         i++;
     }
-    
-    int dp1[size];
-    int dp2[size];
+
+    int dp1[size],  dp2[size]; //vla
     dp1[0] = count;
     i = 0;
     int j = 1;
     while (s[i] != '\0' && j < size) {
         freq1[s[i] - 'a']--;
-        if (freq1[s[i] - 'a'] == 0) {
-            dp1[j] = dp1[j - 1] - 1;
-        } else {
-            dp1[j] = dp1[j - 1];
-        }
+        if (freq1[s[i] - 'a'] == 0)  dp1[j] = dp1[j - 1] - 1;
+        else dp1[j] = dp1[j - 1];
         j++;
         i++;
     }
     dp2[size - 1] = count;
-
     i = size - 1;
     j = size - 2;
     while (i >= 0 && j >= 0) {
         freq2[s[i] - 'a']--;
-        if (freq2[s[i] - 'a'] == 0) {
-            dp2[j] = dp2[j + 1] - 1;
-        } else {
-            dp2[j] = dp2[j + 1];
-        }
+        if (freq2[s[i] - 'a'] == 0) dp2[j] = dp2[j + 1] - 1;
+        else dp2[j] = dp2[j + 1];
         j--;
         i--;
     }
     count = 0;
     int z = 1;
     while(z < size){
-        if(dp2[z-1] == dp1[z]){
-            count++;
-        }
+        if(dp2[z-1] == dp1[z]) count++;
         z++;
     }
     return count;
