f7bc1aadbc585534bc5a7f93404468d3bf0dae35
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-22 20:39:24 +0530

    Time: 0 ms (100%), Space: 9.6 MB (72.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.c
+++ 0042-trapping-rain-water/0042-trapping-rain-water.c
@@ -6,20 +6,14 @@ int trap(int* height, int heightSize) {
     int water = 0;
     int i = 1;
     while (i < heightSize) {
-        if (height[i] > dpi[i - 1]) {
-            dpi[i] = height[i];
-        } else {
-            dpi[i] = dpi[i - 1];
-        }
+        if (height[i] > dpi[i - 1]) dpi[i] = height[i];
+        else dpi[i] = dpi[i - 1];
         i++;
     }
     int j = heightSize -2;
     while(j >= 0){
-            if(height[j] > dpj[j+1]){
-                dpj[j] = height[j];
-            }else{
-                dpj[j] = dpj[j+1];
-            }
+            if(height[j] > dpj[j+1]) dpj[j] = height[j];
+            else dpj[j] = dpj[j+1];
         j--;
     }
     i = 0;
@@ -27,6 +21,5 @@ int trap(int* height, int heightSize) {
         water += fmin(dpi[i],dpj[i])- height[i];
         i++;
     }
-
     return water;
 }
