1236d8d91e8e00794addadb48ffd93b6bfd66d56
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-26 20:52:22 +0530

    Time: 18 ms (18.78%), Space: 15.9 MB (8.59%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -9,18 +9,14 @@ struct hash_entry {
 bool hasCycle(struct ListNode *head) {
     struct hash_entry * hash = NULL;
     while(head){
-        struct hash_entry * node;
+        struct hash_entry * node, *new_node;
         struct ListNode * nd = head;
         HASH_FIND_INT(hash, &nd, node);
-        if(node != NULL){
-            return true;
-        }
-        struct hash_entry * new_node;
+        if(node != NULL)return true;
         new_node = malloc(sizeof(*new_node));
         new_node->nd = head;
         HASH_ADD_INT(hash,nd, new_node);
         head = head->next;
     }
-
     return false;
 }
