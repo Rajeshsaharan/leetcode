b2bf6b4c5ab4584d5f80b02f5a255d80173d44ad
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-09-30 07:39:23 +0530

    Time: 0 ms (100%), Space: 9.8 MB (61.9%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.c
+++ 0075-sort-colors/0075-sort-colors.c
@@ -1,15 +1,9 @@
 void sortColors(int* nums, int numsSize) {
     int arr[3] ={0}; //store color
       int  i =0;
-    while(i< numsSize){
-        arr[nums[i]]++;
-        i++;
-    }
-
-
+    while(i< numsSize) arr[nums[i++]]++;
     i =0;
-    int k =0;
-    int j =0;
+    int k =0,j =0;
     while(i < numsSize){
         while(arr[j] <= 0) j++;
         nums[k++] = j;
