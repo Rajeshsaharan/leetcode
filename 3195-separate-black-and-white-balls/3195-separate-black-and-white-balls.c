1c03a26f91362d952382eb5d0806765099559f79
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-10-05 12:10:34 +0530

    Time: 4 ms (63.89%), Space: 10.7 MB (11.11%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.c
@@ -0,0 +1,13 @@
+long long minimumSteps(char* s) {
+    int count_one = 0;
+    int i = 0;
+    long long int result = 0;
+    while(s[i] != '\0'){
+        if(s[i] == '1') count_one++;
+        else {
+            result += count_one;
+        }
+        i++;
+    }
+    return result;
+}
