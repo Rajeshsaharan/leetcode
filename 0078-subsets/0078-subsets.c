8a603266182f53de3141ff04ab44430982be572e
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-10-07 16:47:10 +0530

    Time: 0 ms (100%), Space: 11 MB (12.28%) - LeetHub

--- 0078-subsets/0078-subsets.c
+++ 0078-subsets/0078-subsets.c
@@ -1,11 +1,12 @@
+int ** result;
+int  * subset;
 
-
-void helper(int * nums, int numsSize, int * returnSize, int ** returnColumnSizes, int index, int * subset, int subsetSize, int *** result){
+void helper(int * nums, int numsSize, int * returnSize, int ** returnColumnSizes, int index, int subsetSize){
     if(index >= numsSize){
-        (*result)[*returnSize] = malloc(sizeof(int) * subsetSize);
+        result[*returnSize] = malloc(sizeof(int) * subsetSize);
         int i =0;
         while(i < subsetSize){
-            (*result)[*returnSize][i] = subset[i];
+            result[*returnSize][i] = subset[i];
             i++;
         }
         (*returnColumnSizes)[(*returnSize)++] = subsetSize;
@@ -13,19 +14,18 @@ void helper(int * nums, int numsSize, int * returnSize, int ** returnColumnSizes
     }
 
     subset[subsetSize] = nums[index];
-    helper(nums, numsSize, returnSize, returnColumnSizes, index+1, subset, subsetSize+1,result);
-    helper(nums, numsSize, returnSize, returnColumnSizes, index+1, subset, subsetSize, result);
+    helper(nums, numsSize, returnSize, returnColumnSizes, index+1,subsetSize+1);
+    helper(nums, numsSize, returnSize, returnColumnSizes, index+1, subsetSize);
 }
 
 
 int** subsets(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
     int capacity = 1 << numsSize;
-    int ** result;
     result = malloc(sizeof(*result) *capacity);
     *returnSize = 0;
     *returnColumnSizes = malloc(sizeof(**returnColumnSizes) * capacity);
-    int * subset = malloc(sizeof(int) * numsSize);
-    helper(nums, numsSize, returnSize, returnColumnSizes, 0, subset, 0, &result);
+    subset = malloc(sizeof(int) * numsSize);
+    helper(nums, numsSize, returnSize, returnColumnSizes, 0,  0);
     free(subset);
     return result;
 
