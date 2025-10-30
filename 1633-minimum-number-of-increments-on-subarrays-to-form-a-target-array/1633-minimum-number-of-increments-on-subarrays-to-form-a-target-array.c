5adc1f2bb0900f1f9bb9cf86bf04a153a0278f8b
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-10-30 06:12:57 +0530

    Time: 0 ms (100%), Space: 15.4 MB (80%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.c
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.c
@@ -1,10 +1,7 @@
 int minNumberOperations(int* target, int targetSize) {
-    int total_operation = target[0];
-    int i = 1;
+    int total_operation = target[0],  i = 1;
     while(i < targetSize){
-        if(target[i] > target[i-1]){
-            total_operation += target[i] - target[i-1];
-        }
+        if(target[i] > target[i-1]) total_operation += target[i] - target[i-1];
         i++;
     }
     return total_operation;
