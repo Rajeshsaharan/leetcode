73212c8574c81237df21786df071e980d9411782
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-10-31 06:52:19 +0530

    Time: 0 ms (100%), Space: 10.5 MB (16.24%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.c
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.c
@@ -1,14 +1,11 @@
 
 
 
-int arr[2];
+int arr[2]; //global arr declare
 int* getSneakyNumbers(int* nums, int numsSize, int* returnSize) {
     int store_arr_freq[101] = {0};
     int i = 0, k = 0;
-    while(i < numsSize){
-        store_arr_freq[nums[i]]++;
-        i++;
-    }
+    while(i < numsSize) store_arr_freq[nums[i++]]++;
     i = 0;
     while(i < numsSize){
         if(store_arr_freq[nums[i]] == 2 && k < 2){
