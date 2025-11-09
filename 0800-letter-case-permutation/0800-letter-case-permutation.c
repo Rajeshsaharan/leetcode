2f62cf6f031c376c544150b0bfd5be2f2ba285fa
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-11-09 17:37:20 +0530

    Time: 19 ms (5%), Space: 14.4 MB (15%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.c
@@ -0,0 +1,43 @@
+char** result;
+
+void helper(char* s, char* buffer, int index, int buffer_size, int* returnSize,
+            int size) {
+    if (index >= size) {
+        buffer[buffer_size] = '\0';
+        printf("buffer_size  %d and buffer %s \n", buffer_size, buffer);
+        result[*returnSize] = malloc(sizeof(char) * (buffer_size + 1));
+        int i = 0;
+        while (buffer[i] != '\0') {
+            result[*returnSize][i] = buffer[i];
+            i++;
+        }
+        result[*returnSize][i] = '\0';
+        (*returnSize)++;
+        return;
+    }
+    if (s[index] >= 48 && s[index] <= 57) {
+        buffer[buffer_size] = s[index];
+        helper(s, buffer, index + 1, buffer_size + 1, returnSize, size);
+        return;
+    } else if(s[index] >= 97 && s[index] <= 122){
+        buffer[buffer_size] = s[index];
+        helper(s, buffer, index + 1, buffer_size + 1, returnSize, size);
+        buffer[buffer_size] = s[index] - 32;
+        helper(s, buffer, index + 1, buffer_size + 1, returnSize, size);
+    }else{
+        buffer[buffer_size] = s[index] +32;
+        helper(s, buffer, index+1, buffer_size +1, returnSize, size);
+        buffer[buffer_size] = s[index];
+        helper(s, buffer, index +1, buffer_size +1 , returnSize, size);
+    }
+}
+
+char** letterCasePermutation(char* s, int* returnSize) {
+    int size = strlen(s);
+    result = malloc(sizeof(*result) * 5000);
+    char* buffer = malloc(sizeof(*buffer) * (size + 1));
+    int index = 0, buffer_size = 0;
+    *returnSize = 0;
+    helper(s, buffer, index, buffer_size, returnSize, size);
+    return result;
+}
