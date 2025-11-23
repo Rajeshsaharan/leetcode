e522f4b8584dd0181d600220663fb9975fd3ea4b
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-11-23 11:04:26 +0530

    Time: 4 ms (5.57%), Space: 9.4 MB (44.18%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.c
@@ -0,0 +1,39 @@
+char ** result;
+void helper(int o, int c, char * buffer, int buffer_size, int * returnSize){
+    if(o == 0 && c == 0){
+        buffer[buffer_size] = '\0';
+        printf("%s and buffer_size is %d\n",buffer, buffer_size);
+         result[*returnSize] = malloc(sizeof(char) * (buffer_size + 1));
+       int i = 0;
+        while (buffer[i] != '\0') {
+            result[*returnSize][i] = buffer[i];
+            i++;
+        }
+        result[*returnSize][i] = '\0';
+        (*returnSize)++;
+        return;
+    }
+    if(o == c){
+        buffer[buffer_size] = '(';
+        helper(o-1, c, buffer, buffer_size +1, returnSize);
+    }else{
+        if(o == 0){
+        buffer[buffer_size] = ')';
+        helper(o, c-1, buffer, buffer_size +1, returnSize);
+        }else{
+            buffer[buffer_size] = '(';
+            helper(o-1, c , buffer, buffer_size +1, returnSize);
+            buffer[buffer_size] = ')';
+            helper(o, c-1, buffer, buffer_size +1, returnSize);
+        }
+    }
+
+
+}
+char** generateParenthesis(int n, int* returnSize) {
+    result = malloc(sizeof(*result) * 10000);
+    char * buffer = malloc(sizeof(*buffer) * (n * 2 +1));
+    int buffer_size = 0; *returnSize = 0;
+    helper(n, n, buffer, buffer_size, returnSize);
+    return result;
+}
