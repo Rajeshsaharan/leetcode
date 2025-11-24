def901f41742d773e3e76c379d49eb94e3055bb0
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-11-24 15:55:53 +0530

    Time: 0 ms (100%), Space: 7.8 MB (61.54%) - LeetHub

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.c
@@ -0,0 +1,9 @@
+char findKthBit(int n, int k) {
+    if(n == 1) return '0'; //base case
+    int length = (1 << n) -1;
+    int mid = length /2 +1;
+    if(k == mid) return '1';
+    if(k < mid) return findKthBit(n-1, k);
+    return findKthBit(n-1, length-k+1) == '0' ? '1' : '0';
+
+}
