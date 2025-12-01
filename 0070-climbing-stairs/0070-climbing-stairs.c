132f63f2d80f143755f89dc58425c170e73b9505
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-01 06:53:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (24.75%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -0,0 +1,18 @@
+
+int dp[46];
+
+int helper(int n) {
+    if (n == 0)
+        return 1;
+    if (n < 0)
+        return 0;
+    if (dp[n] != -1)
+        return dp[n];
+    dp[n] = helper(n - 1) + helper(n - 2);
+    return dp[n];
+}
+
+int climbStairs(int n) {
+    memset(dp, -1, sizeof(dp));
+    return helper(n);
+}
