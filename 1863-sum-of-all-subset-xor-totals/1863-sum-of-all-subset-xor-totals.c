3e504b562edcf1c794481b97147eac11e2ce9cd1
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-01 15:26:42 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.28%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.c
@@ -0,0 +1,15 @@
+int total_xor;
+void helper(int *nums, int numsSize, int current_xor, int index) {
+    total_xor += current_xor;
+    for(int i = index; i < numsSize; i++) {
+        current_xor ^= nums[i];
+        helper(nums, numsSize, current_xor, i + 1);
+        current_xor ^= nums[i];
+    }
+}
+
+int subsetXORSum(int* nums, int numsSize) {
+    total_xor = 0;
+    helper(nums, numsSize, 0, 0);
+    return total_xor;
+}
