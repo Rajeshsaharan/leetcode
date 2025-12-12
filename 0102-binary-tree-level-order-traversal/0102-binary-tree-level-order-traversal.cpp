db24384ea23ad003cc9927bde66e0d24a5dd270f
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-12 19:51:11 +0530

    Time: 0 ms (100%), Space: 16.7 MB (97.15%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,30 +1,28 @@
+
 class Solution {
 private:
-    void dfs(TreeNode * root,int level, vector<int> &v){
+    void dfs(TreeNode * root , vector<vector<int>> &v, int level){
         if(root == NULL) return;
-        if(level == 1){
-            v.push_back(root->val);
-        }else{
-            dfs(root->left, level-1, v);
-            dfs(root->right, level-1, v);
-        }
+        v[level].push_back(root->val);
+        dfs(root->left, v, level +1);
+        dfs(root->right, v, level +1);
     }
 private:
-    int _height(TreeNode * root){
-        if(root == NULL) return 0;
-        int left_height = _height(root->left);
-        int right_height = _height(root->right);
-        return fmax(left_height, right_height) +1;
+    int height(TreeNode * root){
+        if(root== NULL) return 0;
+        int left_height = height(root->left);
+        int right_height = height(root->right);
+        return 1 +max(left_height, right_height);
     }
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-       vector<vector<int>> result;
-       int height = _height(root);
-       for(int i = 1; i <= height ; i++){
-            vector<int> v;
-            dfs(root, i,v);
-            result.push_back(v);
-       } 
-    return result;
+        vector<vector<int>> result;
+        //pre compute value;
+        int h = height(root);
+        for(int i = 0; i< h; i++){
+            result.push_back({});
+        }
+        dfs(root, result, 0);
+        return result;
     }
 };
