cb2b4921c48164b0e69379e2429a2f05f4724942
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-14 05:19:31 +0530

    Time: 0 ms (100%), Space: 8.3 MB (26.74%) - LeetHub

--- 0100-same-tree/0100-same-tree.c
+++ 0100-same-tree/0100-same-tree.c
@@ -2,5 +2,7 @@
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
     if(p == NULL && q == NULL) return true;
     if(p == NULL || q == NULL || p->val != q->val) return false;
-    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);    
+    bool left_decision = isSameTree(p->left, q->left);
+    bool right_decision = isSameTree(p->right, q->right);
+    return left_decision && right_decision;    
 }
