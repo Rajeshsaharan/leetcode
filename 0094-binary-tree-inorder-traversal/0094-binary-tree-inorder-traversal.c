1778110b78686463adb52490f271d4732410f59f
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-15 07:19:07 +0530

    Time: 0 ms (100%), Space: 9.5 MB (6.57%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.c
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.c
@@ -1,16 +1,17 @@
 int * result;
-void helper(struct TreeNode * root, int *returnSize){
+void helper(struct TreeNode * root, int *bs){
     if(root == NULL) return;
-    helper(root->left, returnSize);
-    result[(*returnSize)++] = root->val;
-    helper(root->right, returnSize);
+    helper(root->left, bs);
+    result[(*bs)++] = root->val;
+    helper(root->right, bs);
     return;
 }
 
 
 int* inorderTraversal(struct TreeNode* root, int* returnSize) {
     result = malloc(sizeof(int) * 10000);
-    *returnSize = 0;
-    helper(root, returnSize);
+    int bs = 0;
+    helper(root, &bs);
+    *returnSize = bs;
     return result;
 }
