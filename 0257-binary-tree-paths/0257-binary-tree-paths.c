8effe14786c2366a68f1e474a725cf61f398707f
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-15 05:41:02 +0530

    Time: 2 ms (23.26%), Space: 15.6 MB (5.04%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.c
+++ 0257-binary-tree-paths/0257-binary-tree-paths.c
@@ -1,14 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+
 
 char** result;
 void helper(struct TreeNode* root, char* buf, int bs, int* returnSize) {
