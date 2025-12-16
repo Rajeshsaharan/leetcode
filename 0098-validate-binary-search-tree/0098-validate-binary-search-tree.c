3960a2d70ca4f72b21342c1197b0ad271cc7a52d
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-16 07:34:23 +0530

    Time: 0 ms (100%), Space: 12.1 MB (69.62%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.c
@@ -0,0 +1,11 @@
+bool helper(struct TreeNode * root, long min, long max){
+    if(root == NULL) return true;
+    if(root->val <= min || root->val >= max){
+        return false;
+    }
+    return helper(root->left, min, root->val) && helper(root->right, root->val, max);
+}
+
+bool isValidBST(struct TreeNode* root) {
+    return helper(root, LONG_MIN, LONG_MAX);    
+}
