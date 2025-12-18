9f6feb648e74c5e100130849c2af7a99ec15241d
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-18 07:35:57 +0530

    Time: 0 ms (100%), Space: 19.5 MB (5.54%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.c
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.c
@@ -5,8 +5,8 @@ int comp(const void *a, const void *b) {
 
 void helper(struct TreeNode * root, int * result){
     if(root == NULL) return;
-    result[idx++] = root->val;
     helper(root->left, result);
+    result[idx++] = root->val;
     helper(root->right, result);
 }
 
@@ -14,6 +14,6 @@ int kthSmallest(struct TreeNode* root, int k) {
     idx = 0;
     int * result = malloc(sizeof(int)* 100000);
     helper(root, result);
-    qsort(result, idx, sizeof(result[0]), comp);
+    //qsort(result, idx, sizeof(result[0]), comp);
     return result[k-1];
 }
