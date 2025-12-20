b2895927101ae6ff683dc2bbc88421469bbbc2a4
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-20 17:32:30 +0530

    Time: 92 ms (0%), Space: 106.5 MB (0%) - LeetHub

--- 3319-k-th-largest-perfect-subtree-size-in-binary-tree/3319-k-th-largest-perfect-subtree-size-in-binary-tree.c
+++ 3319-k-th-largest-perfect-subtree-size-in-binary-tree/3319-k-th-largest-perfect-subtree-size-in-binary-tree.c
@@ -2,15 +2,12 @@ struct my_struct {
     int height;
     int size;
 };
-
-
 int idx;
 int * result;
 
 int comp(const void *a, const void *b) {
     return (*(int *)b - *(int *)a);
 }
-
 struct my_struct *  helper(struct TreeNode * root){
     if(root == NULL){
         struct my_struct * st = malloc(sizeof(struct my_struct));
@@ -21,17 +18,15 @@ struct my_struct *  helper(struct TreeNode * root){
     struct my_struct * left = helper(root->left);
     struct my_struct * right = helper(root->right);
     struct my_struct * st = malloc(sizeof(struct my_struct));
-    st->height = 1 + fmax(left->height, right->height);
+    st->height = 1 + (left->height > right->height ? left->height : right->height);
     st->size = 1 + left->size + right->size;
-    int perfect_binary_tree_node = (1<< st->height) -1;
+    int perfect_binary_tree_node = (1LL<< st->height) -1;
     if(st->size == perfect_binary_tree_node){
         result[idx++] = st->size;
     }
     return st; 
     
 }
-
-
 int kthLargestPerfectSubtree(struct TreeNode* root, int k) {
     idx = 0;
     result  = calloc(100000, sizeof(int));
