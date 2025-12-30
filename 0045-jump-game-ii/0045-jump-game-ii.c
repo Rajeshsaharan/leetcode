38acb71c1bda14a4d309a7da34d9c1239bae5033
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-30 07:23:26 +0530

    Time: 232 ms (5.12%), Space: 10.6 MB (11.38%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.c
+++ 0045-jump-game-ii/0045-jump-game-ii.c
@@ -1,18 +1,25 @@
 int dp[100001];
-int helper(int * nums, int index, int numsSize){
-    if(index == numsSize-1 ) return 0;
-    if(index >= numsSize) return 0;
-    if(dp[index] != -1) return dp[index];
-    int i = 1;
-    int min = INT_MAX -1;
-    while(i <= nums[index]){
-        min = fmin(min, helper(nums, index + i, numsSize));
-        i++;
+
+int helper(int *nums, int index, int numsSize) {
+    if (index == numsSize - 1)
+        return 0;
+
+    if (index >= numsSize)
+        return INT_MAX - 1;
+
+    if (dp[index] != -1)
+        return dp[index];
+
+    int minJumps = INT_MAX - 1;
+
+    for (int i = 1; i <= nums[index]; i++) {
+        minJumps = fmin(minJumps,
+                        helper(nums, index + i, numsSize));
     }
-    dp[index] = min +1;
+
+    dp[index] = minJumps + 1;
     return dp[index];
 }
-
 
 int jump(int* nums, int numsSize) {
     memset(dp, -1, sizeof(dp));
