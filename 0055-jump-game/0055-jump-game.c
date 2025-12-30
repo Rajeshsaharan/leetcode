c9ee9ae18d58b4ec545017edbba98e8d84b2d83e
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2025-12-30 06:17:49 +0530

    Time: 489 ms (5.16%), Space: 12 MB (6%) - LeetHub

--- 0055-jump-game/0055-jump-game.c
+++ 0055-jump-game/0055-jump-game.c
@@ -1,20 +1,24 @@
 int dp[100001];
-bool helper(int * nums, int index, int numsSize){
-    if(index == numsSize-1) return true;
-    if(index >= numsSize) return false;
-    if(dp[index] != -1) return dp[index];
-    int i = 1;
-    while(i <= nums[index]){
-        bool flag =  helper(nums, index+ i, numsSize);
-        dp[index] = flag;
-        if(flag) return true;
-        i++;
+
+bool helper(int *nums, int index, int numsSize) {
+    if (index == numsSize - 1) return true;
+    if (index >= numsSize) return false;
+
+    if (dp[index] != -1)
+        return dp[index];
+
+    for (int i = 1; i <= nums[index]; i++) {
+        if (helper(nums, index + i, numsSize)) {
+            dp[index] = true;
+            return true;
+        }
     }
-    return dp[index] = false;
-    
+
+    dp[index] = false;
+    return false;
 }
+
 bool canJump(int* nums, int numsSize) {
     memset(dp, -1, sizeof(dp));
-    //firstly do with recursion 
-     return helper(nums, 0, numsSize);
+    return helper(nums, 0, numsSize);
 }
