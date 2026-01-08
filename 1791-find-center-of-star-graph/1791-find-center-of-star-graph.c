eec23252a7c62c50cf85c57a09f592e2799d2553
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2026-01-08 16:26:01 +0530

    Time: 3 ms (3.05%), Space: 34 MB (68.41%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.c
@@ -0,0 +1,17 @@
+int findCenter(int** edges, int edgesSize, int* edgesColSize) {
+    //chance for looking first and second
+    int first_choice = edges[0][0], first = 0;
+    int second_choice = edges[0][1], second = 0;
+    int i = 0;
+    while(i < edgesSize){
+        int j = 0;
+        while(j < edgesColSize[i]){
+            if(edges[i][j] == first_choice) first++;
+            if(edges[i][j] == second_choice) second++;
+            j++;
+        }
+        i++;
+    }
+    printf("first %d and seconde %d", first, second);
+    return first == edgesSize? first_choice : second_choice;
+}
